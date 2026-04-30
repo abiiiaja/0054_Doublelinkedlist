c77b24fed6411be1afef5d0295785cceea256b33
abiiiaja <kalindraaraya@gmail.com>
2026-04-30 22:08:17 +0700

    Ketika node yang ingin dihapus berada di depan

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -104,6 +104,14 @@ public:
             return;
         }
 
+        // Step 2: If node is at the beginning
+        if (current == START)
+        {
+            START = current->next; // Step 2a: START = START.next
+            if (START != NULL)
+                START->prev = NULL; // Step 2b: START.prev = NULL
+        }
+
     }
 
 };
